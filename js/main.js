ead5b5a352210d2928dfc006f22d570d9e34daf6
daniyakobi <nadeev.danil2016@yandex.ru>
2020-07-30 17:35:54 +0300

    Create: send message

--- js/main.js
+++ js/main.js
@@ -1,10 +1,58 @@
+// Карта
+ymaps.ready(function () {
+  var myMap = new ymaps.Map('map', {
+          center: [7.574017, 79.803766],
+          zoom: 13
+      }, {
+          searchControlProvider: 'yandex#search'
+      }),
+
+      // Создаём макет содержимого.
+      MyIconContentLayout = ymaps.templateLayoutFactory.createClass(
+          '<div style="color: #FFFFFF; font-weight: bold;">$[properties.iconContent]</div>'
+      ),
+
+      myPlacemark = new ymaps.Placemark(myMap.getCenter(), {
+          hintContent: 'Собственный значок метки',
+          balloonContent: 'Это красивая метка'
+      }, {
+          // Опции.
+          // Необходимо указать данный тип макета.
+          iconLayout: 'default#image',
+          // Своё изображение иконки метки.
+          iconImageHref: 'img/place.svg',
+          // Размеры метки.
+          iconImageSize: [30, 42],
+          // Смещение левого верхнего угла иконки относительно
+          // её "ножки" (точки привязки).
+          iconImageOffset: [-5, -38]
+      })
+
+  myMap.geoObjects
+   .add(myPlacemark);
+});
+
 $(document).ready(function() {
   $('.menu-button').click(function(event) {
     $('.menu-button').toggleClass('active-burger');
     $('.mobile-menu').toggleClass('active-menu');
-    $('body').toggleClass('lock');
+    $('body').toggleClass('lock');   
+  });
+
+  $(".navbar-menu").on("click","a", function (event) {
+    $('.menu-button').removeClass('active-burger');
+    $('.mobile-menu').removeClass('active-menu');
+    //отменяем стандартную обработку нажатия по ссылке
+    event.preventDefault();
+    //забираем идентификатор бока с атрибута href
+    var id  = $(this).attr('href'),
+    //узнаем высоту от начала страницы до блока на который ссылается якорь
+    top = $(id).offset().top - $('.navbar').height();
+    //анимируем переход на расстояние - top за 1500 мс
+    $('body,html').animate({scrollTop: top}, 1500);
   });
 });
+
 
 var hotelSlider = new Swiper('.hotel-slider', {
   // Optional parameters
---
+++ send.php
@@ -0,0 +1,58 @@
+<?php
+  // Файлы phpmailer
+  require 'PHPMailer/PHPMailer.php';
+  require 'PHPMailer/SMTP.php';
+  require 'PHPMailer/Exception.php';
+
+  // Переменные, которые отправляет пользователь
+  $name = $_POST['name'];
+  $phone = $_POST['phone'];
+  $message = $_POST['message'];
+
+  // Формирование самого письма
+  $title = "Новое обращение Best Tour Plan";
+  $body = "
+  <h2>Новое обращение</h2>
+  <b>Имя:</b> $name<br>
+  <b>Телефон:</b> $phone<br><br>
+  <b>Сообщение:</b><br>$message
+  ";
+
+  // Настройки PHPMailer
+  $mail = new PHPMailer\PHPMailer\PHPMailer();
+  try {
+      $mail->isSMTP();   
+      $mail->CharSet = "UTF-8";
+      $mail->SMTPAuth   = true;
+      //$mail->SMTPDebug = 2;
+      $mail->Debugoutput = function($str, $level) {$GLOBALS['status'][] = $str;};
+
+      // Настройки вашей почты
+      $mail->Host       = 'smtp.yandex.ru'; // SMTP сервера вашей почты
+      $mail->Username   = 'nadeev.danil2016@yandex.ru'; // Логин на почте
+      $mail->Password   = 'Volko5dav2125asd'; // Пароль на почте
+      $mail->SMTPSecure = 'ssl';
+      $mail->Port       = 465;
+      $mail->setFrom('nadeev.danil2016@yandex.ru', 'Даниил Надеев'); // Адрес самой почты и имя отправителя
+
+      // Получатель письма
+      $mail->addAddress('Katikch21@gmail.com');  
+      $mail->addAddress('erios2015@yandex.ru');  
+
+  // Отправка сообщения
+  $mail->isHTML(true);
+  $mail->Subject = $title;
+  $mail->Body = $body;    
+
+  // Проверяем отравленность сообщения
+  if ($mail->send()) {$result = "success";} 
+  else {$result = "error";}
+
+  } catch (Exception $e) {
+      $result = "error";
+      $status = "Сообщение не было отправлено. Причина ошибки: {$mail->ErrorInfo}";
+  }
+
+  // Отображение результата
+  header('location: thank.html');
+?>
